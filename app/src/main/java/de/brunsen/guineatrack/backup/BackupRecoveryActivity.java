0ce42231387e6f241795fcdd02ffd16a64fc32ad
brunsen <bennet.brunsen@informatik.uni-hamburg.de>
2017-03-19 13:25:30 +0100

    Clean up

--- app/src/main/java/de/brunsen/guineatrack/backup/BackupRecoveryActivity.java
+++ app/src/main/java/de/brunsen/guineatrack/backup/BackupRecoveryActivity.java
@@ -1,33 +1,16 @@
 package de.brunsen.guineatrack.backup;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
-
-import com.tbruyelle.rxpermissions2.Permission;
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 import de.brunsen.guineatrack.R;
-import de.brunsen.guineatrack.database.GuineaPigCRUD;
-import de.brunsen.guineatrack.model.GuineaPig;
 import de.brunsen.guineatrack.ui.activities.BaseActivity;
-import de.brunsen.guineatrack.ui.dialogs.PermissionDialog;
-import de.brunsen.guineatrack.util.JsonExporter;
-import de.brunsen.guineatrack.util.JsonImporter;
-import io.reactivex.functions.Consumer;
 
 public class BackupRecoveryActivity extends BaseActivity implements BackupRecoveryView {
-
-    private static String TAG = BackupRecoveryActivity.class.getName();
 
     @BindView(R.id.backup_button)
     protected Button mBackupButton;
@@ -45,85 +28,26 @@ public class BackupRecoveryActivity extends BaseActivity implements BackupRecove
         initToolbar();
         mUnbinder = ButterKnife.bind(this);
         mToolbar.setTitle(R.string.title_activity_backup_recovery);
-        mRecoveryPresenter = new
+        mRecoveryPresenter = new BackupRecoveryPresenterImpl(this);
+        mRecoveryPresenter.setView(this);
     }
 
     @Override
     protected void onDestroy() {
+        mRecoveryPresenter.clearView();
+        mRecoveryPresenter = null;
         mUnbinder.unbind();
         super.onDestroy();
     }
 
     @OnClick(R.id.backup_button)
     protected void onBackupButtonClicked() {
-        exportGuineaPigs();
+        mRecoveryPresenter.onBackupButtonClicked();
     }
 
     @OnClick(R.id.import_button)
     protected void onImportButtonClicked() {
-        RxPermissions permissions = RxPermissions.getInstance(BackupRecoveryActivity.this);
-        boolean hasExternalReadAccess = permissions.isGranted(android.Manifest.permission.READ_EXTERNAL_STORAGE);
-        if (hasExternalReadAccess) {
-            importGuineaPigs();
-        } else {
-            askForExternalStorageReadAccess();
-        }
-    }
-
-    private void exportGuineaPigs() {
-        JsonExporter exporter = new JsonExporter(this);
-        GuineaPigCRUD crud = new GuineaPigCRUD(this);
-        List<GuineaPig> guineaPigs = crud.getGuineaPigs();
-        try {
-            exporter.exportGuineaPigs(guineaPigs);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void importGuineaPigs() {
-        JsonImporter importer = new JsonImporter(this);
-        try {
-            importer.loadGuineaPigs();
-        } catch (IOException e) {
-            Log.e(TAG, "No file found to import guinea pigs");
-            handleNoFileError();
-        } catch (JSONException e) {
-            Log.e(TAG, "Corrupted import file");
-            handleCorruptedFileError();
-        }
-    }
-
-    private void handleNoFileError() {
-        String title = getString(R.string.error);
-        String message = getString(R.string.error_no_file);
-        String okMessage = getString(android.R.string.ok);
-        showError(title, message, okMessage);
-    }
-
-    private void handleCorruptedFileError() {
-        String title = getString(R.string.error);
-        String message = getString(R.string.error_corrupted_file);
-        String okMessage = getString(android.R.string.ok);
-        showError(title, message, okMessage);
-    }
-
-    private void askForExternalStorageReadAccess() {
-        RxPermissions permissions = RxPermissions.getInstance(this);
-        permissions.requestEach(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                .subscribe(new Consumer<Permission>() {
-                    @Override
-                    public void accept(Permission permission) throws Exception {
-                        if (permission.granted) {
-                            importGuineaPigs();
-                        } else if (permission.shouldShowRequestPermissionRationale) {
-                            String rationaleMessage = getString(R.string.rationale_message_import);
-                            PermissionDialog dialog = new PermissionDialog(BackupRecoveryActivity.this, rationaleMessage, null);
-                            dialog.show();
-                        } else {
-                            // TODO: Inform user about the grave mistake...
-                        }
-                    }});
+        mRecoveryPresenter.onImportButtonClicked();
     }
 
     @Override
--- app/src/main/java/de/brunsen/guineatrack/backup/BackupRecoveryPresenterImpl.java
+++ app/src/main/java/de/brunsen/guineatrack/backup/BackupRecoveryPresenterImpl.java
@@ -1,12 +1,35 @@
 package de.brunsen.guineatrack.backup;
 
 import android.content.Context;
+import android.util.Log;
+
+import com.tbruyelle.rxpermissions2.Permission;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
+import de.brunsen.guineatrack.R;
+import de.brunsen.guineatrack.database.GuineaPigCRUD;
+import de.brunsen.guineatrack.model.GuineaPig;
+import de.brunsen.guineatrack.ui.dialogs.PermissionDialog;
+import de.brunsen.guineatrack.util.JsonExporter;
+import de.brunsen.guineatrack.util.JsonImporter;
+import io.reactivex.functions.Consumer;
 
 public class BackupRecoveryPresenterImpl implements BackupRecoveryPresenter {
+
+    private static String TAG = BackupRecoveryPresenterImpl.class.getName();
 
     private BackupRecoveryView mBackupRecoveryView;
 
     private Context mContext;
+
+    public BackupRecoveryPresenterImpl(Context context) {
+        mContext = context;
+    }
 
     @Override
     public void setView(Object view) {
@@ -15,16 +38,77 @@ public class BackupRecoveryPresenterImpl implements BackupRecoveryPresenter {
 
     @Override
     public void clearView() {
+        mContext = null;
         mBackupRecoveryView = null;
     }
 
     @Override
     public void onImportButtonClicked() {
-
+        RxPermissions permissions = RxPermissions.getInstance(mContext);
+        boolean hasExternalReadAccess = permissions.isGranted(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasExternalReadAccess) {
+            importGuineaPigs();
+        } else {
+            askForExternalStorageReadAccess();
+        }
     }
 
     @Override
     public void onBackupButtonClicked() {
+        exportGuineaPigs();
+    }
 
+    private void exportGuineaPigs() {
+        JsonExporter exporter = new JsonExporter(mContext);
+        GuineaPigCRUD crud = new GuineaPigCRUD(mContext);
+        List<GuineaPig> guineaPigs = crud.getGuineaPigs();
+        try {
+            exporter.exportGuineaPigs(guineaPigs);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }private void importGuineaPigs() {
+        JsonImporter importer = new JsonImporter(mContext);
+        try {
+            importer.loadGuineaPigs();
+        } catch (IOException e) {
+            Log.e(TAG, "No file found to import guinea pigs");
+            handleNoFileError();
+        } catch (JSONException e) {
+            Log.e(TAG, "Corrupted import file");
+            handleCorruptedFileError();
+        }
+    }
+
+    private void handleNoFileError() {
+        String title = mContext.getString(R.string.error);
+        String message = mContext.getString(R.string.error_no_file);
+        String okMessage = mContext.getString(android.R.string.ok);
+        mBackupRecoveryView.showErrorDialog(title, message, okMessage);
+    }
+
+    private void handleCorruptedFileError() {
+        String title = mContext.getString(R.string.error);
+        String message = mContext.getString(R.string.error_corrupted_file);
+        String okMessage = mContext.getString(android.R.string.ok);
+        mBackupRecoveryView.showErrorDialog(title, message, okMessage);
+    }
+
+    private void askForExternalStorageReadAccess() {
+        RxPermissions permissions = RxPermissions.getInstance(mContext);
+        permissions.requestEach(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Permission>() {
+                    @Override
+                    public void accept(Permission permission) throws Exception {
+                        if (permission.granted) {
+                            importGuineaPigs();
+                        } else if (permission.shouldShowRequestPermissionRationale) {
+                            String rationaleMessage = mContext.getString(R.string.rationale_message_import);
+                            PermissionDialog dialog = new PermissionDialog(mContext, rationaleMessage, null);
+                            dialog.show();
+                        } else {
+                            // TODO: Inform user about the grave mistake...
+                        }
+                    }});
     }
 }
