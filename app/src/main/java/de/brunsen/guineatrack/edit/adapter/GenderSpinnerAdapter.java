8b8b9d6453996e189aaeedffe4d4ba3c5f18e357
brunsen <bennet.brunsen@informatik.uni-hamburg.de>
2017-03-25 13:48:21 +0100

    Fixed some lint warnings

--- app/src/main/java/de/brunsen/guineatrack/edit/adapter/GenderSpinnerAdapter.java
+++ app/src/main/java/de/brunsen/guineatrack/edit/adapter/GenderSpinnerAdapter.java
@@ -2,6 +2,7 @@ package de.brunsen.guineatrack.edit.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,7 +37,7 @@ public class GenderSpinnerAdapter extends ArrayAdapter<Gender> {
     }
 
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+    public @NonNull View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View row = convertView;
         if (row == null) {
             LayoutInflater inflater = (LayoutInflater) context
--- app/src/main/java/de/brunsen/guineatrack/edit/adapter/TypeSpinnerAdapter.java
+++ app/src/main/java/de/brunsen/guineatrack/edit/adapter/TypeSpinnerAdapter.java
@@ -2,6 +2,7 @@ package de.brunsen.guineatrack.edit.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,7 +38,7 @@ public class TypeSpinnerAdapter extends ArrayAdapter<Type> {
     }
 
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+    public @NonNull View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View row = convertView;
         if (row == null) {
             LayoutInflater inflater = (LayoutInflater) context
